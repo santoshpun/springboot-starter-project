2ef3090e772dfe309f988c3566b4804c00eb3917
Santosh Pun <santosh@f1soft.com>
2019-12-10 21:03:26 +0545

    first commit

---
+++ src/main/java/com/santosh/springbootstarterproject/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.santosh.springbootstarterproject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Spring boot is working!";
+	}
+
+}
---
+++ src/test/java/com/santosh/springbootstarterproject/SpringbootStarterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosh.springbootstarterproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStarterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
